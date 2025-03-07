444de5b2ccd48598dcaa386177c9d4edf6f71323
holyPaladu <ironic_paladin@mail.ru>
2025-03-07 19:50:16 +0500

    Remove unused Kafka client configuration and add network settings for services in Docker Compose

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -4,7 +4,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
 import { UsersModule } from './users/users.module';
 import { KafkaService } from './kafka/kafka.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { KafkaModule } from './kafka/kafka.module';
 
 @Module({
@@ -13,20 +12,6 @@ import { KafkaModule } from './kafka/kafka.module';
     TypeOrmModule.forRoot(databaseConfig),
     UsersModule,
     KafkaModule,
-    ClientsModule.register([
-      {
-        name: 'KAFKA_PRODUCER',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            brokers: ['kafka:9092'],
-          },
-          consumer: {
-            groupId: 'auth-producer',
-          },
-        },
-      },
-    ]),
   ],
   controllers: [],
   providers: [KafkaService],
